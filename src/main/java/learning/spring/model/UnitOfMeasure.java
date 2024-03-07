893cd0a22bab0de49544d813037fb2f51cda567a
Kondraschov Andrew <kondraschov.andrew1999@gmail.com>
2024-03-07 15:06:31 -0500

    add forgotten property description fo uom

--- src/main/java/learning/spring/model/UnitOfMeasure.java
+++ src/main/java/learning/spring/model/UnitOfMeasure.java
@@ -11,11 +11,21 @@ public class UnitOfMeasure {
     @GeneratedValue(strategy =  GenerationType.IDENTITY)
     private Long id;
 
+    private String description;
+
     public void setId(Long id) {
         this.id = id;
     }
 
     public Long getId() {
         return id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
     }
 }
