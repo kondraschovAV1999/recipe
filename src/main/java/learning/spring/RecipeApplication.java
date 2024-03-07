5af6e3472d1ecb93cfea6e265efdf5def9700364
Kondraschov Andrew <kondraschov.andrew1999@gmail.com>
2024-03-07 15:15:17 -0500

    add bidirectional many-to-many mapping to recipe and category

--- src/main/java/learning/spring/RecipeApplication.java
+++ src/main/java/learning/spring/RecipeApplication.java
@@ -1,4 +1,4 @@
-package learning.spring.recipe;
+package learning.spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/main/java/learning/spring/model/Category.java
+++ src/main/java/learning/spring/model/Category.java
@@ -1,2 +1,39 @@
-package learning.spring.model;public class Category {
+package learning.spring.model;
+
+import jakarta.persistence.*;
+
+import java.util.Set;
+
+@Entity
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String description;
+    @ManyToMany(mappedBy = "categories")
+    private Set<Recipe> recipes;
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Set<Recipe> getRecipes() {
+        return recipes;
+    }
+
+    public void setRecipes(Set<Recipe> recipes) {
+        this.recipes = recipes;
+    }
 }
