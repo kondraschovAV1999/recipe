a175d62d2010898ff069f21022ac2b69c6219ab8
Kondraschov Andrew <kondraschov.andrew1999@gmail.com>
2024-03-12 23:18:35 -0400

    add note and recipe, add one-to-one ralationship

---
+++ src/main/java/learning/spring/model/Recipe.java
@@ -0,0 +1,106 @@
+package learning.spring.model;
+
+import jakarta.persistence.*;
+
+import java.time.LocalTime;
+
+@Entity
+public class Recipe {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String description;
+    private LocalTime prepTime;
+    private LocalTime cookTime;
+    private Integer servings;
+    private String source;
+    private String url;
+    private String directions;
+    //    private Difficulty difficulty;
+    @Lob
+    private Byte[] image;
+    @OneToOne(cascade = CascadeType.ALL)
+    private Note note;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LocalTime getPrepTime() {
+        return prepTime;
+    }
+
+    public void setPrepTime(LocalTime prepTime) {
+        this.prepTime = prepTime;
+    }
+
+    public LocalTime getCookTime() {
+        return cookTime;
+    }
+
+    public void setCookTime(LocalTime cookTime) {
+        this.cookTime = cookTime;
+    }
+
+    public Integer getServings() {
+        return servings;
+    }
+
+    public void setServings(Integer servings) {
+        this.servings = servings;
+    }
+
+    public String getSource() {
+        return source;
+    }
+
+    public void setSource(String source) {
+        this.source = source;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getDirections() {
+        return directions;
+    }
+
+    public void setDirections(String directions) {
+        this.directions = directions;
+    }
+
+    public Byte[] getImage() {
+        return image;
+    }
+
+    public void setImage(Byte[] image) {
+        this.image = image;
+    }
+
+    public Note getNote() {
+        return note;
+    }
+
+    public void setNote(Note note) {
+        this.note = note;
+    }
+}
