af2e2a0f4b1345c89ecf7bb49e2980e89c442b9d
Kondraschov Andrew <kondraschov.andrew1999@gmail.com>
2024-03-15 16:31:26 -0400

    add a from to create a new recipe

--- src/main/java/learning/spring/recipe/controllers/RecipeController.java
+++ src/main/java/learning/spring/recipe/controllers/RecipeController.java
@@ -1,19 +1,36 @@
 package learning.spring.recipe.controllers;
 
+import learning.spring.recipe.dto.RecipeDTO;
 import learning.spring.recipe.service.RecipeService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
 @RequestMapping("/recipe")
 @Controller
 @AllArgsConstructor
 public class RecipeController {
     private final RecipeService recipeService;
-    @RequestMapping({"/show/{id}"})
+
+    @RequestMapping({"{id}/show/","{id}/show"})
     public String showById(@PathVariable Long id, Model model) {
         model.addAttribute("recipe", recipeService.findById(id));
         return "recipe/show";
+    }
+
+    @RequestMapping("new")
+    public String newRecipe(Model model) {
+        model.addAttribute("recipe", new RecipeDTO());
+        return "recipe/recipeform";
+    }
+
+    @PostMapping("/")
+    public String saveOrUpdate(@ModelAttribute RecipeDTO dto) {
+        RecipeDTO savedDto = recipeService.saveRecipeDto(dto);
+        return "redirect:/recipe/show/%d".formatted(savedDto.getId());
     }
 }
--- src/test/java/learning/spring/recipe/controllers/RecipeControllerTest.java
+++ src/test/java/learning/spring/recipe/controllers/RecipeControllerTest.java
@@ -1,5 +1,6 @@
 package learning.spring.recipe.controllers;
 
+import learning.spring.recipe.dto.RecipeDTO;
 import learning.spring.recipe.model.Recipe;
 import learning.spring.recipe.service.RecipeService;
 import org.junit.jupiter.api.BeforeEach;
@@ -8,14 +9,16 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @ExtendWith(MockitoExtension.class)
 class RecipeControllerTest {
@@ -40,6 +43,30 @@ class RecipeControllerTest {
 
         mockMvc.perform(get("/recipe/show/1"))
                 .andExpect(status().isOk())
-                .andExpect(view().name("recipe/show"));
+                .andExpect(view().name("recipe/show"))
+                .andExpect(model().attributeExists("recipe"));
+    }
+
+    @Test
+    void testGetNewRecipeForm() throws Exception {
+        mockMvc.perform(get("/recipe/new"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("recipe/recipeform"))
+                .andExpect(model().attributeExists("recipe"));
+    }
+
+    @Test
+    void testPostNewRecipeFrom() throws Exception {
+        RecipeDTO dto = new RecipeDTO();
+        dto.setId(2L);
+
+        when(recipeService.saveRecipeDto(any())).thenReturn(dto);
+
+        mockMvc.perform(post("/recipe/")
+                        .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                        .param("id", "")
+                        .param("description", "some string"))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(view().name("redirect:/recipe/show/2"));
     }
 }
